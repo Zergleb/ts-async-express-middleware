ec9b9b46138dc7f7b083fc1aef96abf1d9ea65c0
Zergleb <boneless1213@gmail.com>
2019-02-22 13:49:15 -0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export * from './lib/async';
-export * from './lib/hash';
-export * from './lib/number';
+import { NextFunction, Request, Response } from 'express';
+
+const asyncMiddleware = (
+  fn: (req: Request, res: Response, next: NextFunction) => void
+) => (req: Request, res: Response, next: NextFunction) => {
+  /* istanbul ignore next  */
+  Promise.resolve(fn(req, res, next)).catch(err => {
+    /* istanbul ignore next  */
+    next(err);
+  });
+};
+
+export default asyncMiddleware;
